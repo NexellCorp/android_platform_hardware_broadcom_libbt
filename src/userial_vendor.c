17c10b13247d4b2b7f9dd1ade4d3aec4be0d4b99
Lee Geunjeong <kjlee@nexell.co.kr>
2019-07-26 15:03:17 +0900

    libbt: add stable time before changing uart speed.
    In communication between bcm43455 and Nexell AP,
    stable time is needed when changing uart speed.
    
    This patch adds stable time when changing uart speed.
    This patch reverts sco packet size to its original size once.
    
    Signed-off-by: Lee Geunjeong <kjlee@nexell.co.kr>
    Change-Id: I45ae5f3470b4c58285ce658f851e9796ab699242

--- src/userial_vendor.c
+++ src/userial_vendor.c
@@ -249,6 +249,7 @@ int userial_vendor_open(tUSERIAL_CFG *p_cfg)
     }
 
     tcflush(vnd_userial.fd, TCIOFLUSH);
+    usleep(150000);
 
     tcgetattr(vnd_userial.fd, &vnd_userial.termios);
     cfmakeraw(&vnd_userial.termios);
@@ -322,6 +323,7 @@ void userial_vendor_set_baud(uint8_t userial_baud)
     cfsetospeed(&vnd_userial.termios, tcio_baud);
     cfsetispeed(&vnd_userial.termios, tcio_baud);
     tcsetattr(vnd_userial.fd, TCSANOW, &vnd_userial.termios);
+    usleep(150000);
 }
 
 /*******************************************************************************
